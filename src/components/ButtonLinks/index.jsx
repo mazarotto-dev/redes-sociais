6f15f6d8cbf728f76672ebc8a2890ae6b062b183
Julio Cesar Mazarotto Sulato <mazarottoop@gmail.com>
2023-07-29 19:54:37 -0300

    Atualizando as redes sociais

--- src/components/ButtonLinks/index.jsx
+++ src/components/ButtonLinks/index.jsx
@@ -4,9 +4,9 @@ import style from './style.module.css'
 export default function AllLinks() {
     return (
         <main className={style.mainLinks}>
-            <Links link='https://github.com/mazarottoo'>GitHub</Links>
-            <Links link='https://instagram.com/mazarottoo'>Instagram</Links>
-            <Links link='https://linkedin.com/in/mazarottoo'>LinkedIn</Links>
+            <Links link='https://github.com/mazarotto-dev'>GitHub</Links>
+            <Links link='https://instagram.com/mazarotto.dev'>Instagram</Links>
+            <Links link='https://linkedin.com/in/mazarotto-dev'>LinkedIn</Links>
             <Links link='https://wa.me/5514998676097?text=Ol%C3%A1%21+Eu+gostaria+de+um+site...'>Contato</Links>
         </main>
     )
